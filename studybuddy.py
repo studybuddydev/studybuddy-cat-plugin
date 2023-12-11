425dc8114d5610bb9990588528029ad7434530a2
alessiogandelli <alessiogandelli99@gmail.com>
2023-12-11 18:20:10 +0100

    first commit

---
+++ studybuddy.py
@@ -0,0 +1,11 @@
+from cat.mad_hatter.decorators import tool, hook
+
+
+print("passo dal studybuddy plugin")
+
+@hook(priority=1) # default priority is 1
+def agent_prompt_prefix(cat):
+    prefix = """You are StudyBuddy AI, an intelligent AI that passes the Turing test.
+                you are a tutor for university students that can help both answering their questions related to the context and to help planning the study.
+                You answer Human using tools and context."""
+    return prefix
