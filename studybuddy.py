0307a45eea8bb812c465aa9b01c821f24fcab735
alessiogandelli <alessiogandelli99@gmail.com>
2024-03-22 17:58:47 +0100

    update prompt exam

--- studybuddy.py
+++ studybuddy.py
@@ -4,7 +4,6 @@ from cat.mad_hatter.decorators import hook
 
 print("passo dal studybuddy plugin")
 
-esami = ['storia', 'cruccolo', 'sapobully']
 
 @hook(priority=1) # default priority is 1
 def agent_prompt_prefix(prefix, cat):
@@ -14,39 +13,39 @@ def agent_prompt_prefix(prefix, cat):
     return prefix
 
 
+esami = ['storia', 'gattologia', 'sapobully']
 
-
-@hook(priority=1)  # default priority = 1
-def before_rabbithole_splits_text(doc, cat):
-    # do whatever with the doc
-    print("I'm in the before_rabbithole_splits_text hook")
-    print(doc.text)
-    return doc
-
-@hook(priority=1)  # default priority = 1
-def after_rabbithole_splitted_text(chunks, cat):
-    # post process the chunks
-    print("I'm in the after_rabbithole_splitted_text hook")
-    print(chunks)
-
-    return chunks
-
-
-@tool(return_direct=True)
+@tool(return_direct=False)
 def get_esami(input, cat):
-    """Get exams from the exams list. User may ask "What exams do I have to prepare for?" or similar."""
-    
-    print('eccomi nel tool getesami')
+    """
+        return the list of exams to prepare for.
+  
+        Example inputs:
+        "which exams do I have to prepare for"
+        "what exams do I have to prepare for"
+        "what are the exams i have to do?"
 
+  
+    """
+    print('eccomi nel tool getesami') 
     return "You have to prepare for the following exams: " + str(esami)
 
 
 @tool(return_direct=True)
-def add_esame(exam, cat):
-    """Add exam to the exam list. User may say "add exam" or similar. Argument "exam" is the name of the exam to add."""
-    esami.append(exam)
+def add_esame(name, cat):
+    """Add exam to the exam list. Replies when user say "add exam" or similar, add exam only if it is clear the user want to add an exam. Argument "name" is the name of the exam to add."""
+    esami.append(name)
     
-    return "Exam added "+ exam
+    return "Exam added "+ name
+
+
+
+
+@tool(return_direct=True)
+
+
+
+
 
 
 
